22edf4d35705ad9e2296c4c4cd934eb756437ecd
blackwolf449 <tacvini@gmail.com>
2024-01-20 16:42:36 -0300

    add page system

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CardsOfItensComponent } from '../cards-of-itens/cards-of-itens.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardsOfItensComponent, CommonModule],
+  imports: [CardsOfItensComponent, CommonModule, FormsModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -13,20 +14,44 @@ export class ListComponent {
    @Input() routeTarget: any
    listOfItems = []
    timeOutForANewRequest: any
+   page: number = 0
+   totalOfPages: number = 1
+   searchInput: string = ''
 
-    async searchByNameOnInput($event: any) {
+    async searchByNameOnInput() {
       clearTimeout(this.timeOutForANewRequest)
       this.timeOutForANewRequest = setTimeout(async () =>{
-        const target = $event.target.value
-        const request = await fetch(`https://eldenring.fanapis.com/api/${this.routeTarget.split(" ")[0]}?name=${target}`)
+        const request = await fetch(`https://eldenring.fanapis.com/api/${this.routeTarget.split(" ")[0]}?name=${this.searchInput}`)
         const data = await request.json()
+        this.totalOfPages = this.verifyTotalOfPages(data.total, 20)
         this.listOfItems = data.data
+        this.page = 0
       }, 500) 
+   }
+
+   async changePage(newPage: number) {
+    if(newPage > this.totalOfPages || newPage < 0) return
+    const request = await fetch(`https://eldenring.fanapis.com/api/${this.routeTarget.split(" ")[0]}?name=${this.searchInput}&page=${newPage}`)
+    const data = await request.json()
+    this.page = newPage
+    this.listOfItems = data.data
+   }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth'})
+  }
+
+   verifyTotalOfPages(totalCount: number, limitPerPage: number){
+      let totalPages: number = totalCount / limitPerPage
+      if(totalPages % 1 === 0) return totalPages - 1
+      return parseInt((totalPages).toFixed()) - 1
    }
    
    async ngOnChanges(){
     const request = await fetch(`https://eldenring.fanapis.com/api/${this.routeTarget.split(" ")[0]}`)
     const data = await request.json()
+    this.page = 0
+    this.totalOfPages = this.verifyTotalOfPages(data.total, 20)
     this.listOfItems = data.data
    }
 }
