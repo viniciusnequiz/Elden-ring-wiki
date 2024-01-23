1e6844cd9ccb2702591387af7db09aca7f60875a
blackwolf449 <tacvini@gmail.com>
2024-01-23 14:32:46 -0300

    table items component add

--- src/app/components/table-items/table-items.component.ts
+++ src/app/components/table-items/table-items.component.ts
@@ -1,18 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-items',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './table-items.component.html',
   styleUrl: './table-items.component.scss'
 })
 export class TableItemsComponent {
   @Input() objWithTheInformations: any
-
-  ngOnInit() {
-  console.log(this.objWithTheInformations)
-
-  }
-
 }
