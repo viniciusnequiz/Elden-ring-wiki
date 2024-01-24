a499d23f841ff7a721d97b20b7f00da787f986ee
blackwolf449 <tacvini@gmail.com>
2024-01-24 15:44:48 -0300

    table finished

--- src/app/components/table-items/table-items.component.ts
+++ src/app/components/table-items/table-items.component.ts
@@ -11,10 +11,20 @@ import { Component, Input } from '@angular/core';
   styleUrl: './table-items.component.scss'
 })
 export class TableItemsComponent {
-  @Input() objWithTheInformations: any
+  @Input() objWithTheInformations: any 
+  teste: any = [1, 2]
 
   typeofItem(item:any){
-    console.log(typeof item)
     return typeof item
+  }
+
+  objectToArray(obj: any){
+    const keys = Object.keys(obj)
+    const values = Object.values(obj)
+    const objInArray = []
+    for(let i = 0; i < keys.length; i++){
+        objInArray.push({key: keys[i], value: values[i]})
+    }
+    return objInArray
   }
 }
